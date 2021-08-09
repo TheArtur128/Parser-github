e575bcc43a0615ed9b1c71b6a16a23e922899b67
TheArtur128 <s9339307190@gmail.com>
2021-08-09 23:08:55 +0700

    GUI system

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
+from win32api import GetSystemMetrics as my_win
+from random import randint as random
 import requests
+import tkinter
 
 class User:
     HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
@@ -63,3 +66,72 @@ class User:
     def name(self): return self.__name
     @property
     def repositories(self): return self.__repositories
+
+
+c = { #colors
+    "fone": "#BAD4FF",
+    "button": "#2F36FF",
+    "text": "#031129",
+    "White": "White"
+}
+
+#Оформления окна
+win = tkinter.Tk()
+win.title("Parser-github")
+win.config(bg=c["fone"])
+
+#Геометрия окна
+display = [my_win(0)//4, my_win(1)//2]
+win.geometry(f"{display[0]}x{display[1]}") #I have 341x384!
+win.resizable(False, False)
+
+#Логика
+#Заполняем все колонны
+win.grid_columnconfigure(0, minsize=display[0]//6)
+
+#Команды
+def return_in_data():
+    Participant = User(Entry_URL.get())
+    Entry_URL.delete(0, "end")
+
+    print(f"\n{Participant.name}:\n{Participant.repositories}")
+
+def URL_set():
+    if Entry_URL.get() != "https://github.com/":
+        Entry_URL.insert(0, "https://github.com/")
+
+#Брашура
+tkinter.Label(win, text=f"URL:",
+    bg=c["button"], fg=c["White"],
+    font=("Lucida Sans Unicode",10),
+    width=7, height=1,
+    relief=tkinter.RAISED,
+    bd=0
+).grid(row=0, column=0, stick="nwe")
+
+#Система ввода URL
+Entry_URL = tkinter.Entry(win, relief=tkinter.RAISED, bd=0)
+Entry_URL.grid(row=0, column=1, stick="news")
+
+#Кнопка return
+tkinter.Button(win, text="return URL",
+    command=return_in_data,
+    bg=c["White"], fg=c["button"],
+    activebackground=c["button"],
+    font=("Lucida Sans Unicode",10),
+    relief=tkinter.RAISED,
+    bd=1
+).grid(row=1, column=0, stick="nswe", columnspan=2)
+
+#Подгонка под адрес github'а
+tkinter.Button(win, text="pattern",
+    command=URL_set,
+    bg=c["White"], fg=c["button"],
+    activebackground=c["button"],
+    font=("Lucida Sans Unicode",10),
+    relief=tkinter.RAISED,
+    bd=1
+).grid(row=0, column=2, stick="nsew", rowspan=2)
+
+#Update
+win.mainloop()
